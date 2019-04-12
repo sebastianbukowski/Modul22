fc8fc1e47661b03f04b16ae59680a8b6f0faf16b
SebastianBukowski <seb-27@o2.pl>
2019-04-12 22:19:23 +0200

    Add arrow -s & -m functional generator

---
+++ components/Hello/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Hello = (props, context) => {
+  return (
+    <h1>hello</h1>
+  );
+}
+
+Hello.propTypes = {
+};
+
+export default Hello;
